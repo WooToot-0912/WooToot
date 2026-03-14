0f6bd6e6823199830cd59d8b6a8525a19c3c0229
WooToot-0912 <1950383511@qq.com>
2026-03-14 21:19:51 +0800

    Fix: 删除未使用的 React 导入

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+// 删掉这行多余的导入 ↓
+// import React from 'react';
 import { Navigation } from './components/Navigation';
 import { Hero } from './components/Hero';
 import { About } from './components/About';
